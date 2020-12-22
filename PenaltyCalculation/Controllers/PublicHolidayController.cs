11b9cf006c1ebf39d9ba3bc36b7d242747993e18
ABarcin <74905523+ABarcin@users.noreply.github.com>
2020-12-22 15:38:33 +0300

    public holiday view added

---
+++ PenaltyCalculation/Controllers/PublicHolidayController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using PenaltyCalculation.Models;
+using PenaltyCalculation.Service;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PenaltyCalculation.Controllers
+{
+    public class PublicHolidayController : Controller
+    {
+        private readonly IProcessingService _context;
+        public PublicHolidayController(IProcessingService processingService)
+        {
+            _context = processingService;
+        }
+    
+        public IActionResult Index()
+        {
+            var countries = _context.GetAllCountries();
+            var model = new PublicHolidayModel();
+            foreach (var item in countries)
+                model.Countries.Add(new SelectListItem { Text = item.CountryName, Value = item.Id.ToString() });
+            return View("Index", model);
+        }
+        [HttpPost]
+        public IActionResult Holiday(PublicHolidayModel model)
+        {
+            var publicHoldiays = _context.GetAllHolidaysByCountryId(model.SelectedCountryId);
+            var publicHolidaysDate = new List<DateTime>();
+            foreach (var item in publicHoldiays)
+            {
+                publicHolidaysDate.Add(item.HolidayDate);
+            }
+            model.PublicHolidays = publicHolidaysDate;
+            return View("PublicHolidayView", model);
+        }
+       
+
+    }
+}
---
+++ PenaltyCalculation/Models/PublicHolidayModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace PenaltyCalculation.Models
+{
+    public class PublicHolidayModel
+    {
+        public PublicHolidayModel()
+        {
+            Countries = new List<SelectListItem>();
+        }
+        public string CountryName { get; set; }
+        public List<DateTime> PublicHolidays { get; set; }
+        public IList<SelectListItem> Countries { get; set; }
+        [DisplayName("Select Country For Public Holiday")]
+        public int SelectedCountryId { get; set; }
+    }
+}
